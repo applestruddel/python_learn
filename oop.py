a3950776f015652dcd857f210e8ec5a5a3c73f81
Tristan <tristan@burda.us>
2014-12-03 12:36:16 -0600

    into  to oop

---
+++ oop.py
@@ -0,0 +1,20 @@
+class yolo:
+    empcount=0
+    def _init_(self, name, salary):
+        self.name = name
+        self.salary = salary
+        yolo.empcount += 1
+
+    def displaycount(self):
+        print "total Emp %d" % yolo.empcount
+
+    def displayemp(self):
+        print "Name: ",self.name, "sal:", self.salary
+
+
+emp1 = yolo(chuck, 5)
+emp2 = yolo(dill, 6)
+
+
+emp1.displayemp()
+emp2.displayemp()
